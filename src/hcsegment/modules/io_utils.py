1f57d1d6a47a21d596b4eeb133da862f8eaf1c61
cviets <cviets@mgh.harvard.edu>
2026-07-16 11:46:24 -0400

    Some stylistic changes; also allow stitching with different numbers of wells

--- src/hcsegment/modules/io_utils.py
+++ src/hcsegment/modules/io_utils.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Set
 from collections.abc import Iterable
 import re
 import os
@@ -7,11 +7,9 @@ import numpy as np
 from tqdm import tqdm
 import copy
 import tifffile
-import zarr
-from iohub.ngff import open_ome_zarr
 import csv
 
-def get_positions(file_list: List[str]) -> Tuple[List[str], int, int]:
+def get_positions(file_list: List[str]) -> Tuple[Set[str], Set[str], Set[str]]:
 
     wells = set()
     channels = set()
@@ -37,7 +35,7 @@ def get_positions(file_list: List[str]) -> Tuple[List[str], int, int]:
                 channels = {1}
                 search_for_channels = False
 
-    return sorted(list(wells)), sorted(list(sites)), sorted(list(channels))
+    return wells, sites, channels
 
 def convert_position_list(positions: List[str]) -> List[Tuple[str]]:
     out = [("", "", "")]*len(positions)
@@ -117,24 +115,7 @@ def sort_files(file_list: List[str], positions: List[str], sites: List[str], cha
     return out
 
 def get_grid_position(site: int, rows: int, cols: int) -> Tuple[int]:
-    # i = 0
-    # j = 0
-    # cur_site = 1
-    # while site != cur_site:
-    #     cur_site += 1
-    #     # if i at border, increment j
-    #     if (i==0 and j%2==1) or (i==(cols-1) and j%2==0):
-    #         j += 1
-    #     else:
-    #         # if j even, increment i; else, decrement i
-    #         if j%2 == 0:
-    #             i += 1
-    #         else:
-    #             i -= 1
-    #     assert i >= 0 and i < cols
-    #     assert j >= 0 and j < rows, (j, site)
 
-    
     j = (site-1) // cols
     i = (site-1) % cols
 
@@ -143,27 +124,18 @@ def get_grid_position(site: int, rows: int, cols: int) -> Tuple[int]:
     
     return (j, i)
 
-def get_files_in_path(inp_path: str) -> Tuple[List[str], str]:
+def get_files_in_path(inp_path: str) -> List[str]:
     """
-    Given the input path, returns the images in the path and their format (tiff or zarr)
+    Given the input path, returns the images in the path
     """
     if not os.path.isdir(inp_path):
-        return [], "_"
+        return []
     out = []
     inp_path = os.path.expanduser(inp_path)
     tiffs = glob(os.path.join(inp_path, "*.tif"))
     tiffs.extend(glob(os.path.join(inp_path, "*.tiff")))
 
-    if len(tiffs) == 0:
-        subdirs = [elt for elt in os.listdir(inp_path) if os.path.isdir(elt)]
-        for subdir in subdirs:
-            subsubdirs = [elt for elt in os.listdir(subdir) if os.path.isdir(elt)]
-            for subsubdir in subdirs:
-                out.append(os.path.join(inp_path, subdir, subsubdir, "0", "0"))
-
-        return out, "zarr"
-    else: 
-        return tiffs, "tiff"
+    return tiffs
     
 def get_stitched_images(path_to_stitched_images: str) -> List[str]:
     path_to_stitched_images = os.path.expanduser(path_to_stitched_images)
@@ -187,19 +159,15 @@ def remove_completed_wells(image_list, completed_mask_list, image_path) -> List[
 def save_tiff(data, filename):
     tifffile.imwrite(filename, data)
 
-def read_image(path_to_img: str, format: str) -> np.ndarray:
-    assert format in {"tiff", "zarr"}, "Only tiff and zarr reading supported"
-    if format == "tiff":
-        try:
-            return np.array(tifffile.imread(path_to_img))
-        except:
-            raise Exception(f"Could not open {path_to_img}")
-    else:
-        return np.array(zarr.open(path_to_img))
+def read_image(path_to_img: str) -> np.ndarray:
+    try:
+        return np.array(tifffile.imread(path_to_img))
+    except:
+        raise Exception(f"Could not open {path_to_img}")
     
-def write_image(data: np.ndarray, output: str, well: str, format: str) -> None:
+def write_image(data: np.ndarray, output: str, well: str) -> None:
     """
-    Write image data in tiff or zarr format
+    Write image data in tiff format
 
     Parameters
     -----------------
@@ -209,31 +177,11 @@ def write_image(data: np.ndarray, output: str, well: str, format: str) -> None:
         directory to save images 
     well : str
         name of well on 384-well plate 
-    format : str 
-        tiff or zarr 
     """
     
-    assert format in {"tiff", "zarr"}, "Only tiff and zarr writing supported"
-    output = os.path.expanduser(output)
-    if format == "zarr":
-        with open_ome_zarr(
-            store_path=output,
-            layout='hcs',
-            mode='a'
-        ) as dataset:
-            row = well[0]
-            col = well[1:]
-            position = dataset.create_position(row, col, "0")
-            img = position.create_zeros(
-                name="0", 
-                shape=data.shape, 
-                dtype=data.dtype
-                )
-            img[:] = copy.deepcopy(data)
-    elif format == "tiff":
-        if not os.path.isdir(output):
-            os.mkdir(output)
-        tifffile.imwrite(os.path.join(output, well+".tiff"), data)
+    if not os.path.isdir(output):
+        os.mkdir(output)
+    tifffile.imwrite(os.path.join(output, well+".tiff"), data)
 
 def get_wellname_from_imagepath(image_path):
     pattern = r'\.tif'
